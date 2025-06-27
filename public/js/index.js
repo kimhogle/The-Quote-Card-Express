4456260dddcc4bafaf99830cf755b47009411524
Kim Hogle <kimberly.senior89@yahoo.com>
2025-06-27 15:32:16 -0400

    random image update

--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,30 @@ const elements = {
 };
 
 
+
+
+async function getRandomImage() {
+    const client_id = "AATLU7WYiV-ZtnJljaLMJrB-8YZfnCKgMr40VgFX_LQ";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getRandomImage();
+
+
+console.log("Loaded Unsplash image:", receivedPhotoUrl);
+
+
+/*
 const quotes = [
     {
         quote: "When there’s no more room in hell, the dead will roam the Earth.",
@@ -21,7 +45,7 @@ const quotes = [
         quote: "I will say now, however objectively, that human teleportation, molecular decimation, breakdown, reformation, is inherently purging.",
         author: "Seth Brundle, The Fly (1986)",
     },
-    
+
     {
         quote: "The lesson I want you to learn is: It doesn't matter what you look like. You can be tall or short or fat or thin, or ugly or handsome, like your father, or you can be black or yellow or white. It doesn't matter. But what does matter is the size of your heart and the strength of your character.",
         author: "Herman Munster, The Munsters (1965)",
@@ -53,4 +77,4 @@ function loopThroughQuotes() {
 }
 
 
-setTimeout(loopThroughQuotes, 3000);
+setTimeout(loopThroughQuotes, 3000); */
